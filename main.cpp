facdeae8203950e030f674fd56b1b962a68b3a38
Joel Verhagen <joel.verhagen@gmail.com>
2011-03-16 12:07:09 -0400

    Initial commit.

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <vector>
+using namespace std;
+
+#include "ResourceFile.h"
+
+using namespace std;
+
+int main()
+{
+    {
+        // write test
+        cout << "Write Test:" << endl;
+
+        ResourceFileWriter writeTest("writeTest.dat");
+        writeTest.addEntry("ball.png", "files/ball.png");
+        writeTest.addEntry("audio.mp3", "files/audio.mp3");
+        writeTest.addEntry("Text Document.txt", "files/Text Document.txt");
+        writeTest.write();
+
+        vector<string> entryNamesList = writeTest.getEntryNames();
+
+        cout << "The following files were added to writeTest.dat." << endl;
+        for(vector<string>::iterator i = entryNamesList.begin(); i != entryNamesList.end(); i++)
+        {
+            string entryName = (*i);
+            cout << entryName << endl;
+        }
+    }
+
+    cout << endl << endl;
+
+    {
+        // read test
+        cout << "Read Test:" << endl;
+
+        ResourceFileReader readTest("writeTest.dat");
+        readTest.read();
+
+        vector<string> entryNamesList = readTest.getEntryNames();
+
+        cout << "The following files arte in writeTest.dat." << endl;
+        for(vector<string>::iterator i = entryNamesList.begin(); i != entryNamesList.end(); i++)
+        {
+            string entryName = (*i);
+            cout << entryName << " (" << readTest.getEntryFileSize(entryName) << " bytes)" << endl;
+        }
+    }
+
+    return 0;
+}
